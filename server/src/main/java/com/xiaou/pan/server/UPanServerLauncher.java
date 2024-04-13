da7147b3b3c0e838cb1e94e6eaeab67ab870c855
aliyun1153623907 <3153566913@qq.com>
2024-04-13 15:25:17 +0800

    mybatis

--- server/src/main/java/com/xiaou/pan/server/UPanServerLauncher.java
+++ server/src/main/java/com/xiaou/pan/server/UPanServerLauncher.java
@@ -2,22 +2,30 @@ package com.xiaou.pan.server;
 
 import com.xiaou.pan.core.constants.RPanConstants;
 import com.xiaou.pan.core.response.R;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
 
 @SpringBootApplication(scanBasePackages = RPanConstants.BASE_COMPONENT_SCAN_PATH)
 @ServletComponentScan(basePackages = RPanConstants.BASE_COMPONENT_SCAN_PATH)
 @RestController
+@Validated
+@EnableTransactionManagement
+@MapperScan(RPanConstants.BASE_COMPONENT_SCAN_PATH + ".server.modules.**.mapper")
 public class UPanServerLauncher {
     public static void main(String[] args) {
         SpringApplication.run(UPanServerLauncher.class, args);
     }
 
     @GetMapping("hello")
-    public R<String> hello(String name) {
+    public R<String> hello(@NotBlank(message = "name不能为空") String name) {
         return R.success("hello" + name);
     }
 }
